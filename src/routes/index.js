013ff8a114b7c8541290d71975b3d8de9e030cc7
Roberto Arruda <betoakang@hotmail.com>
2022-05-20 18:00:37 -0300

    refactor: remove useless comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,9 @@
 import {
   BrowserRouter as Router, Route, Routes as Switch,
 } from 'react-router-dom';
-// import { useAuth } from '../contexts/auth';
-// import { Home } from '../pages/Home';
 import Login from '../pages/Login';
-// import { Signup } from '../pages/Signup';
 
 export default function Routes() {
-//   const { isAuthenticate } = useAuth();
-
   return (
     <Router>
       <Switch>

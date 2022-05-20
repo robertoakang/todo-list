c5301644196d8e306c3d21f36fc395505ac3f640
Roberto Arruda <betoakang@hotmail.com>
2022-05-20 20:07:41 -0300

    feat: add Login page

---
+++ src/components/Form/index.js
@@ -0,0 +1,141 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import PropTypes from 'prop-types';
+
+function Form({ properties }) {
+  const [inputedValues, setInputedValues] = useState({});
+
+  const {
+    title, fields, link, handle,
+  } = properties;
+
+  function handleChange(valueField, field) {
+    if (field) {
+      const obj = {};
+      obj[field] = valueField;
+      setInputedValues((value) => ({ ...value, ...obj }));
+    }
+  }
+
+  async function handleForm(event) {
+    event.preventDefault();
+    handle(inputedValues);
+  }
+
+  return (
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        bgcolor: 'white',
+        boxShadow: '0 5px 10px 0 rgb(0 0 0 / 10%)',
+        borderRadius: '10px',
+      }}
+    >
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          paddingTop: 3,
+          paddingBottom: 5,
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          {title}
+        </Typography>
+        <Box component="form" onSubmit={handleForm} validate sx={{ mt: 1 }}>
+          {
+              Object.entries(fields).map(([field, value]) => {
+                const {
+                  autoComplete,
+                  autoFocus,
+                  id,
+                  inputType,
+                  label,
+                  name,
+                  required,
+                  type,
+                } = value;
+
+                switch (inputType) {
+                  case 'TextField':
+                    return (
+                      <TextField
+                        margin="normal"
+                        required={required}
+                        fullWidth
+                        id={id}
+                        label={label}
+                        name={name}
+                        autoComplete={autoComplete}
+                        autoFocus={autoFocus}
+                        type={type}
+                        onChange={(e) => handleChange(e.target.value, id)}
+                        key={`textfield-${id + field}`}
+                      />
+                    );
+                  case 'Button':
+                    return (
+                      <Button
+                        type={type}
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        key={`button-${id + field}`}
+                      >
+                        {label}
+                      </Button>
+                    );
+                  default:
+                    return (
+                      <div />
+                    );
+                }
+              })
+            }
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Link href={link.route} variant="body2">
+                {link.text}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
+
+Form.propTypes = {
+  properties: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    fields: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+      required: PropTypes.string.boolean,
+      id: PropTypes.string,
+      label: PropTypes.string,
+      autoComplete: PropTypes.string,
+      autoFocus: PropTypes.string.boolean,
+      type: PropTypes.string,
+      inputType: PropTypes.string,
+    })).isRequired,
+    link: PropTypes.string,
+    handle: PropTypes.func.isRequired,
+  }),
+};
+
+Form.defaultProps = {
+  properties: {},
+};
+
+export default Form;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,49 @@
+import React from 'react';
+import Form from '../../components/Form';
+
 export default function Login() {
+  async function handleLogin(fields) {
+    const { email, password } = fields;
+    console.log({ email, password });
+  }
+
   return (
-    <div>
-      LOGIN
-    </div>
+    <Form
+      properties={{
+        title: 'LOGIN',
+        fields: [
+          {
+            name: 'Email',
+            required: true,
+            id: 'email',
+            label: 'Email',
+            autoComplete: 'email',
+            autoFocus: true,
+            type: 'input',
+            inputType: 'TextField',
+          },
+          {
+            name: 'Password',
+            required: true,
+            id: 'password',
+            label: 'Senha',
+            autoComplete: 'current-password',
+            autoFocus: false,
+            type: 'password',
+            inputType: 'TextField',
+          },
+          {
+            type: 'submit',
+            inputType: 'Button',
+            label: 'Entrar',
+          },
+        ],
+        link: {
+          route: '/signup',
+          text: 'Não tem uma conta? Cadastre-se.',
+        },
+        handle: handleLogin,
+      }}
+    />
   );
 }

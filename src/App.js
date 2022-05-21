45a8a6d93a36cda22ce9f2556fcc9e388063187f
Roberto Arruda <betoakang@hotmail.com>
2022-05-20 22:13:56 -0300

    chore: add Prettier

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import 'react-toastify/dist/ReactToastify.css';
 import Routes from './routes';
 
 export default function App() {
-  return (
-    <div className="App">
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <Routes />
-    </div>
-  );
+	return (
+		<div className="App">
+			<ToastContainer
+				position="top-right"
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
+			<Routes />
+		</div>
+	);
 }
